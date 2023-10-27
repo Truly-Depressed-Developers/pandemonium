3c294f88bd5aa08e8b007fc39ffe0fd25a82ad6c
Twarug <patryk280502@gmail.com>
2023-10-28 01:32:54 +0200

    Create GameController

---
+++ Assets/Scripts/Core/GameController.cs
@@ -0,0 +1,19 @@
+﻿using Cutscenes;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Core {
+    public class GameController : MonoSingleton<GameController> {
+        
+        [SerializeField] private Cutscene startCutscene;
+        
+        private void Start() {
+            if (!MainMenu.MainMenu.HasBeenLoaded) {
+                SceneManager.LoadScene(0);
+                return;
+            }
+            
+            Debug.Log("GameStart");
+        }
+    }
+}
--- Assets/Scripts/MainMenu/MainMenu.cs
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -1,11 +1,20 @@
+using System;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace MainMenu {
     public class MainMenu : MonoBehaviour {
+        
+        public static bool HasBeenLoaded { get; private set; }
+
+        private void Start() {
+            HasBeenLoaded = true;
+        }
+
         #region Callback
 
         public void Callback_NewGame() {
-            Debug.Log("TODO: New Game");
+            SceneManager.LoadScene(sceneBuildIndex: 2);
         }
 
         public void Callback_Exit() {
