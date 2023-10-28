5eede2198378825cf83b1ac7aa9db0621dc33668
Michał Ślęzak <michaldakol@gmail.com>
2023-10-28 07:01:54 +0200

    nie dziala

---
+++ Assets/DamageSystem/FreezeInvoker.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace DamageSystem {
+    public class FreezeInvoker : MonoBehaviour {
+
+        [SerializeField] private LayerMask freezeReceivers;
+        [SerializeField] private float freezeTime;
+        private IEnumerator delayedCoroutine = null;
+        Action<bool> unFreeze;
+
+        public void CancelFreeze() {
+            StopCoroutine(delayedCoroutine);
+            unFreeze(false);
+        }
+
+        private void OnCollisionEnter2D(Collision2D other) {
+            Debug.Log("1");
+            if (freezeReceivers != (freezeReceivers | 1 << other.gameObject.layer)) return;
+            Debug.Log("2");
+            other.gameObject.TryGetComponent(out FreezeReceiver freezeReceiver);
+            Debug.Log("3");
+            if (!freezeReceiver) return;
+            Debug.Log("4");
+            Freeze(freezeReceiver);
+        
+            //TakeDamage(damageDealer.GetDamage());
+        }
+
+        private void OnTriggerEnter2D(Collider2D other) {
+            if (GetComponent<Weapon>()) return;
+            if (freezeReceivers != (freezeReceivers | 1 << other.gameObject.layer)) return;
+            FreezeReceiver freezeReceiver = other.gameObject.GetComponentInParent<FreezeReceiver>();
+            if (freezeReceiver == null) return;
+            Freeze(freezeReceiver);
+
+            //TakeDamage(damageDealer.GetDamage());
+        }
+
+        private void Freeze(FreezeReceiver freezeReceiver) {
+            unFreeze = freezeReceiver.Freeze();
+            delayedCoroutine = DelayedUnfreeze();
+            StartCoroutine(delayedCoroutine);
+        }
+
+        private IEnumerator DelayedUnfreeze() {
+            yield return new WaitForSeconds(freezeTime);
+            unFreeze(true);
+        }
+
+    }
+}
---
+++ Assets/DamageSystem/FreezeReceiver.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace DamageSystem {
+    public class FreezeReceiver : MonoBehaviour {
+
+        public UnityEvent<bool> onFreezeEnd;
+        public UnityEvent onFreeze;
+        
+        public Action<bool> Freeze() {
+            onFreeze.Invoke();
+            Debug.Log("Freezed");
+            return Unfreeze;
+        }
+
+        public void Unfreeze(bool finished) {
+            Debug.Log("Funreezed");
+
+            onFreezeEnd.Invoke(finished);
+        }
+    }
+}
