cd655122ed444bb54472e3f73cda9e66097f0cc9
Spookyless <spookyless.info@gmail.com>
2023-10-29 11:51:12 +0100

    Changes pt. 2

--- Assets/Scripts/Morok/Attack.cs
+++ Assets/Scripts/Morok/Attack.cs
@@ -1,24 +1,71 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using Unity.Mathematics;
+using Projectiles;
 using UnityEngine;
-using Vector2 = System.Numerics.Vector2;
 
 namespace Morok {
     public class Attack : MonoBehaviour {
-        [SerializeField] private GameObject bulletPrefab;
+        [SerializeField] private GameObject projectilePrefab;
+        [SerializeField] private ProjectileConfig projectileConfig;
+
+        [SerializeField] private float spawnAttackCooldown = 1f;
+        [SerializeField] private float projectileAttackCooldown = 1.5f;
+
+        [SerializeField] private float aerialAttackRepeats = 3f;
+        [SerializeField] private float aerialAttackRepeatCooldown = 0.4f;
+        [SerializeField] private float aerialAttackCooldown = 0.7f;
+
+        private List<Transform> projectileSpawnPoints = new();
+        private Transform target;
         
         private void Start() {
+            Transform projectileSpawnPointsRoot = transform.GetChild(0).transform;
+
+            foreach (Transform child in projectileSpawnPointsRoot) {
+                projectileSpawnPoints.Add(child);
+            }
+            
+            GameObject t = GameObject.FindGameObjectWithTag("Player");
+            if (!t) t = GameObject.Find("Player");
+
+            if (t) {
+                target = t.transform;
+            }
+            
             StartCoroutine(AttackCoroutine());
         }
 
         private IEnumerator AttackCoroutine() {
             while (true) {
-                foreach (var vect2 in new List<Vector3>(){new (0, 1), new (1, 0), new (1, 1), new (0, 0)}) {
-                    GameObject bullet = Instantiate(bulletPrefab, transform.position + 3 * vect2, quaternion.identity);  
-                }
-                yield return new WaitForSeconds(2);
+                yield return new WaitForSeconds(spawnAttackCooldown);
+                
+                if (!target) break;
+
+                float roll = Random.value;
+
+                if (roll <= 1f) yield return ProjectileAttack();
+            }
+        }
+
+        private IEnumerator ProjectileAttack() {
+            foreach (var spawnPoint in projectileSpawnPoints) {
+                GameObject projectile = Instantiate(projectilePrefab, spawnPoint.transform.position, Quaternion.identity);
+                Projectile projectileScript = projectile.GetComponent<Projectile>();
+
+                Vector2 direction = (target.position - projectile.transform.position).normalized;
+                    
+                projectileScript.SetDirection(direction);
+                if(projectileConfig) projectileScript.AssignConfig(projectileConfig);
+            }
+            
+            yield return new WaitForSeconds(projectileAttackCooldown);
+        }
+
+        private IEnumerator AerialAttack() {
+            for (int i = 0; i < aerialAttackRepeats; i++) {
+                if (!target) break;
+
+                yield return new WaitForSeconds(1f);
             }
         }
     }
